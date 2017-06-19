972ea797e7b2277b6e7871f0a9a264ae74e0cfa8
aayushgoyal1 <goyalaayush@yahoo.com>
2017-06-18 20:35:44 -0500

    Create ApplicationService.java

---
+++ ApplicationService.java
@@ -0,0 +1,52 @@
+import java.util.List;
+
+public interface ApplicationService {
+
+	/**
+	 * Return all applications regardless of user.
+	 * @return
+	 */
+    public List<Application> findAll();
+
+    /**
+     * Returns all rows in the application table for specified registered_by user.
+     * @return
+     */
+    public List<Application> findApplicationsByRegisteredUser(String registered_by);
+   
+    /**
+     * Check if the application exists for the specified appId.
+     * @param appId
+     * @return
+     */
+    public Application findApplicationByAppId(String appId);
+ 
+    /**
+     * Check if the application already exists.
+     * @param app
+     * @return
+     */
+    public boolean isApplicationExist(Application app);
+    
+    /**
+     * Deletes a record in the Application table that matches appId.  Returns # of rows affected.
+     * @param appId
+     * @return
+     */
+    public int delete(String appId);
+ 
+    /**
+     * Creates a record in the Application table.
+     * @param app
+     * @return
+     */
+    public Application create(final Application app);
+    
+    /**
+     * Updates a record in the Application table.
+     * @param app
+     * @return
+     */
+    public Application update(final Application app);
+
+}
