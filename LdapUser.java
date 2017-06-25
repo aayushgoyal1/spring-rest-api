664f87d4dfab46711702dca60802875458300154
Aayush Goyal <goyalaayush@yahoo.com>
2017-06-25 16:55:45 -0500

    Update LdapUser.java

--- LdapUser.java
+++ LdapUser.java
@@ -1,1 +1,36 @@
+import java.util.List;
+
+public class LdapUser {
+
+private String email;
+private String uid;
+private String firstName;
+private String lastName;
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getUid() {
+	return uid;
+}
+public void setUid(String uid) {
+	this.uid = uid;
+}
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+
+}
 
