52cb4700f2f971f467683fe21dd2ce66a4b2df30
Aayush Goyal <goyalaayush@yahoo.com>
2017-06-28 09:28:12 -0500

    Update LoggingService.java

--- service/LoggingService.java
+++ service/LoggingService.java
@@ -1,1 +1,18 @@
+public interface LoggingService {
 
+	public void info (Class<?> sourceClass, String message);
+	
+	public void info (Class<?> sourceClass, String message, Object... args);
+	
+    public void debug (Class<?> sourceClass, String message);
+
+    public void debug (Class<?> sourceClass, String message, Object... args);
+
+    public void error (Class<?> sourceClass, String message);
+
+    public void error (Class<?> sourceClass, String message, Object... args);
+
+    public void trace (Class<?> sourceClass, String message);
+
+    public void trace (Class<?> sourceClass, String message, Object... args);
+}
